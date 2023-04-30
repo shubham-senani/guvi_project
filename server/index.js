9314dc80cec93fa80ec80de867d94e6959ba8c05
skipperr09 <shubhamsenani9@gmail.com>
2023-04-30 17:11:47 +0530

    v3

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ connectToMongod();
 
 const app = express();
 app.use(express.json())
-const port = 8080 || process.env.PORT;
+const port = process.env.PORT || 8080 ;
 app.use(cors())
 
 app.get('/',(req, res)=>{
